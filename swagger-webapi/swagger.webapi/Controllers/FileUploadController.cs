e4db85a408ecae03413b8b2899d1d6e20265b054
Eloi <eloi.rui@gmail.com>
2017-09-24 15:46:59 +0100

    swagger-webapi

---
+++ swagger-webapi/swagger.webapi/Controllers/FileUploadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace swagger.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public IActionResult FileUpload2(IFormFile file)
+        {
+
+
+            //open File
+            //var stream = file.OpenReadStream();
+            //var name = Path.GetFileName(file.FileName);
+
+            return Ok(file.FileName);
+
+        }
+    }
+}
---
+++ swagger-webapi/swagger.webapi/Utils/FileUploadOperation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+using System.Reflection;
+
+namespace swagger.webapi.Utils
+{
+    public class FileUploadOperation : IOperationFilter
+    {
+        //old swagger verison
+        //public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        //{
+        //    if (operation.OperationId.ToLower().Contains("fileupload")) //will add the File upload button in UI for every operation that contains the name "fileupload"
+        //    {
+        //        operation.Consumes.Add("multipart/form-data");
+        //        operation.Parameters.Add(
+        //            new BodyParameter
+        //            {
+        //                name = "file",
+        //                @in = "formData",
+        //                required = true,
+        //                type = "file"
+        //            });
+        //    }
+        //}
+
+        private const string FormDataMimeType = "multipart/form-data";
+        private static readonly string[] FormFilePropertyNames =
+            typeof(IFormFile).GetTypeInfo().DeclaredProperties.Select(x => x.Name).ToArray();
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (context.ApiDescription.ParameterDescriptions.Any(x => x.ModelMetadata.ModelType == typeof(IFormFile)))
+            {
+                operation.Parameters.Clear();//Clearing parameters
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "File",
+                    In = "formData",
+                    Description = "The file to upload.",
+                    Required = true,
+                    Type = "file"
+                });
+                if (!operation.Consumes.Contains(FormDataMimeType))
+                {
+                    operation.Consumes.Add(FormDataMimeType);
+                }
+            }
+        }
+    }
+}
