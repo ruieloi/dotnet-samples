8761b91571c3938dd70e7cc948bca37461923438
Eloi <eloi.rui@gmail.com>
2017-10-16 20:44:49 +0100

    hangfire samples

---
+++ hangfire/HangfireSample/HangfireWebAPI/Controllers/dashboardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hangfire;
+using Hangfire.Storage;
+
+namespace HangfireWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class DashboardController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var monitoringApi = JobStorage.Current.GetMonitoringApi();
+
+           
+
+            var isSucceed = monitoringApi.SucceededJobs(0, System.Convert.ToInt32((int) monitoringApi.SucceededListCount()));
+
+            var details = isSucceed.FirstOrDefault().Value.Job;
+
+           
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ hangfire/HangfireSample/HangfireWebAPI/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Hangfire;
+
+namespace HangfireWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddHangfire(x => x.UseSqlServerStorage("Initial Catalog=hangfire;data source=.\\SQLEXPRESS;integrated security=False;user id=sa;encrypt=False;password=P2ssw0rd;"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+        }
+    }
+}
